49fd3bb014231a4d21b4d2847fe35267527341a2
sahithi-Reddy-0901 <gavinollasahithireddy09@gmail.com>
2026-02-04 12:10:25 +0530

    Add files via upload

---
+++ week-2/array.java
@@ -0,0 +1,14 @@
+class array 
+{
+	public static void main(String[] args) 
+	{
+		// Declare and initialize the array
+		int[] numbers={10,20,30,40,50};
+        // Display the array elements
+		System.out.println("the array elements are");
+		for(int i=0;i<numbers.length;i++)
+		{
+			System.out.println(numbers[i]);
+		}
+	}
+}
