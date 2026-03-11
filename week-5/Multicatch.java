bfeb569435abad3687d036bb898a2e697999bfb9
sahithi-Reddy-0901 <gavinollasahithireddy09@gmail.com>
2026-03-11 12:16:52 +0530

    Add files via upload

---
+++ week-5/Multicatch.java
@@ -0,0 +1,27 @@
+class Multicatch
+{
+	public static void main(String[] args) 
+	{
+	try
+	{
+		int a = args.length;
+		System.out.println("a = " + a);
+		int b=42/a;
+		System.out.println(b);
+	    int c[] = {1};
+	    c[42]=10;
+	}
+	catch(ArithmeticException e)
+	{
+        System.out.println("Divide by 0 VJIT: "+ e);
+	}
+	catch(ArrayIndexOutOfBoundsException e)
+	{
+	    System.out.println("Array index out of Bounds: "+ e);
+	}
+	catch(Exception e){
+        System.out.println("Divide by 0: "+ e);
+	}
+	System.out.println("After try/catch blocks.");
+	}
+}
